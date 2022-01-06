7434fc0176c79880131cf8c175db5672ebc58686
kwaktai <taikwak@me.com>
2022-01-06 14:42:43 +0900

    20220106-3

--- passdoc/pw.py
+++ passdoc/pw.py
@@ -1,4 +1,8 @@
 def passWord():
     return "rhkr5241^^"
 
+
+def slackT():
+    # return "xoxb-1731659647985-2908198700470-LxqWPX24mddkCs6NbYlMStEq"
+    pass
 # print(passWord())
---
+++ slack_engin.py
@@ -0,0 +1,46 @@
+import requests
+from slacker import Slacker
+from passdoc import pw
+from def_loggin import __get_logger
+logger = __get_logger()
+
+
+slack_stock = pw.slackT()
+
+
+channelName = "#unlimited-purchase-vr"
+# channelName = "#stock"
+slackTest = Slacker(slack_stock)
+# channelList = slackTest.channels.list()
+# print(channelList)
+
+
+def slackSendMsg(text):
+    requests.post("https://slack.com/api/chat.postMessage",
+                  headers={"Authorization": "Bearer "+slack_stock},
+                  data={"channel": channelName, "text": text}
+                  )
+    return print((f"message : {text}"))
+
+
+def slackSendFile(img, type=""):
+    try:
+        slackTest.files.upload(f"img/{img}_{type}.png", channels=channelName)
+    except:
+        # logger.info("파일 없습니다.")
+        pass
+
+
+def slackSendMsg_rsi_check(text):
+    requests.post("https://slack.com/api/chat.postMessage",
+                  headers={"Authorization": "Bearer "+slack_stock},
+                  data={"channel": "#rsi-check", "text": text}
+                  )
+    return print((f"channel #rsi-check: {text}"))
+
+
+if __name__ == "__main__":
+    slackSendMsg("?????")
+    slackSendMsg_rsi_check("성공")
+    # slackSendFile("kwak", "거치식")
+    pass
